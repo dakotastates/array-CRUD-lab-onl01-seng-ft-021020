9ffe8aad4ab9be8e8a4bd99625483d0d97435136
Dakota States <dakota.states@gmail.com>
2020-02-11 23:05:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("tacos")
+  foods.push("tacos")
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("coffee")
+  drinks.unshift("coffee")
 end
 
 def remove_element_from_end_of_array(array)
